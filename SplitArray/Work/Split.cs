e497a3368256620732224ff6028f059c67988bc9
dfd <fdf>
2020-10-09 21:19:38 +0300

    Добавьте файлы проекта.

---
+++ SplitArray/Work/Split.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SplitArray.Work
+{
+   public class Split
+    {
+        public IEnumerable<IGrouping<int,T>> Start<T>(IEnumerable<T> enumerator,int count_split)
+        {
+            int count = 0;
+            return enumerator.GroupBy(x =>
+            {
+                if (count_split < count) count = 0;
+                count++;
+                return count;
+            }).ToList();
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SplitArray.Work;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Split split = new Split();
+            var result = split.Start(Enumerable.Range(0, 100), 19);
+            int count = 0;
+            foreach (var item in result)
+            {
+                foreach (var ccc in item)
+                {
+                    Console.WriteLine(ccc);
+                }
+                Console.WriteLine(new string('-',30));
+            }
+            Console.WriteLine("Ok");
+            Console.ReadKey();
+        }
+    }
+}
