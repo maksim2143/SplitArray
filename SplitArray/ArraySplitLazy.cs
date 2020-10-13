6e0a0beb6a759e8a9f54abd26cb0c8689a369c8c
dfd <fdf>
2020-10-13 21:13:35 +0300

    update

--- SplitArray/ArraySplitLazy.cs
+++ SplitArray/ArraySplitLazy.cs
@@ -8,21 +8,21 @@ namespace SplitArray
 {
     public static partial class ArraySplit
     {
-        public static IEnumerable<IGrouping<int, T>> SplitLazy<T>(this IEnumerable<T> enumerable, int count_split)
-        {
-            return split_lazy.Value.Start(enumerable, count_split);
-        }
         static readonly Lazy<Split> split_lazy = new Lazy<Split>();
         static readonly Lazy<Multiplication> multiplication_lazy = new Lazy<Multiplication>();
         static readonly Lazy<Subtract> subtract = new Lazy<Subtract>();
         static readonly Lazy<Addition> addition = new Lazy<Addition>();
+        public static IEnumerable<IGrouping<int, T>> SplitLazy<T>(this IEnumerable<T> enumerable, int count_split)
+        {
+            return split_lazy.Value.Start(enumerable, count_split);
+        }
         public static IEnumerable<IEnumerable<T>> MultiplicationLazy<T>(this IEnumerable<T> enumerable, int count_multiplication) where T : ICloneable
         {
-            return multiplication_lazy.Value.StartThread(enumerable, count_multiplication);
+            return multiplication_lazy.Value.Start(enumerable, count_multiplication);
         }
         public static IEnumerable<T> SubtractLazy<T>(this IEnumerable<T> enumerable, int count_subtract)
         {
-            return subtract.Value.StartThread(enumerable, count_subtract);
+            return subtract.Value.Start(enumerable, count_subtract);
         }
         public static IEnumerable<T> AdditionLazy<T>(this IEnumerable<T> enumerable, int count) where T : ICloneable
         {
